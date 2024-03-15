c5d249d08e2fbdaa119c5c2c97367fe02e17fcd5
Venfi <venfioranai@gmail.com>
2024-03-15 20:40:28 +0200

    Created interface for request store

---
+++ src/request-store.ts
@@ -0,0 +1,9 @@
+import { Subject } from 'rxjs';
+
+export interface RequestStore<Input, Output> {
+  key: string;
+  latestRequest?: Input;
+  latestResponse?: Output;
+  listener: Subject<Output>;
+  activeRequest?: Subject<Output>;
+}
