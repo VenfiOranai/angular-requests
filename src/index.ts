0c176579ca4c700f5a3e15aec4d99f97fb3cc782
Venfi <venfioranai@gmail.com>
2024-03-19 21:47:20 +0200

    Removed method request imports in index

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,3 @@ export { RequestStore } from './request-store';
 export { BaseRequest } from './base-request';
 export { RequestMethod } from './request-methods';
 export { hashObject, sortObjectKeys, ensureObservation } from './utils';
-
-export { GetRequest } from './method-requests/get-request';
-export { PostRequest } from './method-requests/post-request';
-export { PutRequest } from './method-requests/put-request';
-export { DeleteRequest } from './method-requests/delete-request';
-export { PatchRequest } from './method-requests/patch-request';
-export { HeadRequest } from './method-requests/head-request';
-export { OptionsRequest } from './method-requests/options-request';
